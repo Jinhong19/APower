5278e83d8a8b138c068d014d61800b54def07ce8
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-03 22:21:11 -0400

    created model for database

--- communityService/model/community.js
+++ communityService/model/community.js
@@ -1,39 +1,50 @@
 const db = require("../db");
 
 const Schema = db.Schema;
-const communitySchema = new Schema({
-    Name: {type:String, require:true},
-    creator: {type:Number, require:true},
-    detail: {type:String, default: 'This community owner is lazy.'},
-    public: {type:Boolean, require:true},
-    members: [{type:Number}],
-    rulebook: {type:Number},
-    numberOfMembers: {type:Number}
-}, {
-});
+const communitySchema = new Schema(
+  {
+    communityName: { type: String, require: true },
+    creatorId: { type: String, require: true },
+    detail: { type: String, default: "This community owner is lazy." },
+    isPublic: { type: Boolean, require: true },
+    memberIdList: [{ type: String }],
+    adminIdList: [{ type: String }],
+    rulebook: { type: String },
+  },
+  {}
+);
 
-function createCommunity(name, creator, detail, public, members, rulebook){
-    const Community = db.model('Community',communitySchema);
+async function createCommunity(name, creator, detail, public, rulebook) {
+  const Community = db.model("Community", communitySchema);
 
-    // check if community name already in use
-    Community.findOne({'name' : name}, function (err, community){  
-        if (err) return "Community name already been used";
-    })
+  // check if community name already in use
+  await Community.exists({ name: name }, function (err, result) {
+    console.log(result);
+    if (result) return "Community name already been used";
+    else console.log("caaaaaaa");
+  });
 
-    // if not in use, create and add to db
-    Community.create({
-        name:name,
-        creator:creator,
-        detail:detail,
-        public:public,
-        members:[],
-        rulebook:rulebook,
-        numberOfMembers:1
-    }, function (err, community){
-        if (err) return handleError(err);
-    });
+  // if not in use, create and add to db
+  await Community.create(
+    {
+      communityName: name,
+      creatorId: creator,
+      detail: detail,
+      isPublic: public,
+      memberIdList: [creator],
+      adminIdList: [],
+      rulebook: rulebook,
+    },
+    function (err, community) {
+      if (err) return handleError(err);
+    }
+  );
 
-    console.log("community: " + name + " created");
+  return "community: " + name + " created";
 }
 
-module.exports.community = db.model('Community',communitySchema);
+async function joinCommunity(communityName, userName) {}
+
+module.exports.community = db.model("Community", communitySchema);
+module.exports.create_Community = createCommunity;
+module.exports.join_Community = joinCommunity;
