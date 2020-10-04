7f83f8dfbd7016012a0f8887fe36bd1e18a70af9
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-04 19:33:44 -0400

    finished basic database function

--- communityService/model/community.js
+++ communityService/model/community.js
@@ -6,11 +6,43 @@ const communitySchema = new Schema({
     creatorId: {type:String, require:true},
     detail: {type:String, default: 'This community owner is lazy.'},
     isPublic: {type:Boolean, require:true},
-    memberIdList: [{type:String}],
-    adminIdList: [{type:String}],
+    memberIdList: [{
+        type:String
+    }],
+    adminIdList: [{
+        memberId: {type:String}
+    }],
     rulebook: {type:String},
 }, {
 });
+
+async function assignMember(communityName,userId){
+    Community.findOneAndUpdate({'communityName' : communityName},{  $push: {'memberIdList' : userId} }, await function (err, success){
+        if (err) return handleError(err);
+        else return success;
+    });
+}
+
+async function assignAdmin(communityName,userId){
+    Community.findOneAndUpdate({'communityName' : communityName},{  $push: {'adminIdList' : userId} }, await function (err, success){
+        if (err) return handleError(err);
+        else return success;
+    });
+}
+
+async function removeMember(communityName, userId){
+    Community.findOneAndUpdate({'communityName' : communityName},{  $pull: {'memberIdList' : userId} }, await function (err, success){
+        if (err) return handleError(err);
+        else return success;
+    });
+}
+
+async function removeAdmin(communityName, userId){
+    Community.findOneAndUpdate({'communityName' : communityName},{  $pull: {'adminIdList' : userId} }, await function (err, success){
+        if (err) return handleError(err);
+        else return success;
+    });
+}
 
 async function createCommunity(name, creator, detail, public, rulebook){
     const Community = db.model('Community',communitySchema);
@@ -19,11 +51,10 @@ async function createCommunity(name, creator, detail, public, rulebook){
     await Community.exists({name : name}, function (err, result){ 
         console.log(result); 
         if (result) return "Community name already been used";
-        else console.log("caaaaaaa");
     })
 
-    // if not in use, create and add to db
-    await Community.create({
+    // create and add to db
+    const communty = new Community({
         communityName:name,
         creatorId:creator,
         detail:detail,
@@ -31,7 +62,9 @@ async function createCommunity(name, creator, detail, public, rulebook){
         memberIdList:[creator],
         adminIdList:[],
         rulebook:rulebook,
-    }, function (err, community){
+    });
+
+    await communty.save(function (err){
         if (err) return handleError(err);
     });
 
@@ -39,17 +72,22 @@ async function createCommunity(name, creator, detail, public, rulebook){
 }
 
 async function joinCommunity(communityName,userId){
-    
+    assignMember(communityName, userId);
 }
 
 async function quitCommunity(communityName,userId){
+    const Community = db.model('Community',communitySchema);
 
+    removeMember(communityName,userId);
+    removeAdmin(communityName,userId);
 }
 
-async function removeMember(communityName,userId,removeUserId){
-
+async function assignAdmins(communityName,userId){
+    assignAdmin(communityName,userId);
 }
 
 module.exports.community = db.model('Community',communitySchema);
 module.exports.create_Community = createCommunity;
 module.exports.join_Community = joinCommunity;
+module.exports.quit_Community = quitCommunity;
+module.exports.assign_Admin = assignAdmins;
