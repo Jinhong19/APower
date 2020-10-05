a02ef08884cb1d39a65bf3a06f2f736a8c3b7daa
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-04 23:37:27 -0400

    added admin asscees

--- communityService/routes/community.js
+++ communityService/routes/community.js
@@ -167,6 +167,59 @@ router.post('/searchCommunity', function(req,res){
     });
 });
 
+router.post('/assignAdmin', function(req,res){
+    var Community = community.community;
+
+    if(req.body.userId == req.body.assignUserID)
+        res.send("you can't assign yourself to be an admin");
+    else{
+        Community.findOne({'communityName' : req.body.communityName}, function(err,result){
+            if(result){
+                if(result.memberIdList.includes(req.body.assignUserID) == false)
+                    res.send('user is not in the community');
+                else if (result.adminIdList.includes(req.body.assignUserID))
+                    res.send('user is already a admin');
+                else if(result.creatorId !== req.body.userId)
+                    res.send("you don't have authority to assign user as admin");
+                else{
+                    community.assign_Admin(req.body.communityName, req.assignUserID);
+                    res.send('assign success');
+                }
+            }
+            else
+                res.send('community does not exists');
+        });
+    }
+});
+
+router.post('/removeAdmin', function(req,res){
+    var Community = community.community;
+
+    Community.findOne({'communityName' : req.body.communityName, 'adminIdList' : req.body.removeAdminId}, function(err, result){
+        if(result){
+            if(result.creatorId == req.body.userId || req.body.userId == req.body.removeAdminId){
+                community.removeAdminId(communityName,req.body.removeAdminId);
+                res.send('user has been remove from admin list')
+            } 
+        else
+            res.send('user is not an admin')
+        }
+    });
+});
+
+router.post('/abdicate', function(req,res){
+    var Community = community.community;
+
+    Community.exists({'communityName' : req.body.communityName, 'creatorId' : req.body.userId, 'memberIdList' : req.body.newCreatorId}, function(err,result){
+        if(result){
+            community.assignCreator(communityName,req.body.newCreatorId);
+            res.send('new creator roll assigned');
+        }
+        else
+            res.send('unable to assign');
+    });
+});
+
 router.post('/createGameRoom', function(req,res){
     res.send('calling game service');
 });
