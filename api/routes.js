d556022840604fce21389367a8defb89e0f34113
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-08 23:07:43 -0400

    remove api folder

--- api/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const controller = require("./controller");
-
-module.exports = function (app) {
-    app.route("/about").get(controller.about);
-    app.route("/dice").get(controller.dice);
-};
