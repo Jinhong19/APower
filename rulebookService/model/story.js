7236bbf79305bc862dcbf18946ec40b891964ec9
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-22 03:14:57 -0400

    update story schema

--- rulebookService/model/story.js
+++ rulebookService/model/story.js
@@ -3,6 +3,11 @@ const db = require("../db");
 const Schema = db.Schema;
 const storySchema = new Schema({
     stroyName: {type:String, require:true},
-    rulebookId: {type:String, require:true}
+    sotry: {
+        name : {type:String, require:true},
+        data : {type:Buffer, require:true},
+        encoding : {type:String, require:true},
+        size: {type:Number, require:true}
+    }
 }, {
 });
