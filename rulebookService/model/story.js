a4677c9c0f629dde06e3ca2875cbf30ba700c4d3
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-22 04:01:00 -0400

    update story schema

--- rulebookService/model/story.js
+++ rulebookService/model/story.js
@@ -1,13 +1,17 @@
+const { model, models } = require("mongoose");
 const db = require("../db");
 
 const Schema = db.Schema;
 const storySchema = new Schema({
-    stroyName: {type:String, require:true},
-    sotry: {
+    storyName: {type:String, require:true},
+    story: {
         name : {type:String, require:true},
         data : {type:Buffer, require:true},
         encoding : {type:String, require:true},
         size: {type:Number, require:true}
-    }
+    },
+    ruleBookId: {type:String, require:true}
 }, {
 });
+
+module.exports.story = db.model('Story', storySchema);
