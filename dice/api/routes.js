ce7de208f435d004225219a1e133f35594613c1c
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-30 03:25:42 -0400

    remove all file other than communityService

--- dice/api/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const controller = require("./controller");
-module.exports = function (app) {
-  app.get("/about", controller.about);
-  app.get("/dice", controller.dice);
-};
--- dice/service/dice.js
+++
@@ -1,8 +0,0 @@
-var dice = {
-  getNumber: function (req, res, next) {
-    const number = Math.floor(Math.random() * Math.floor(100)) + 1;
-    res.send({ number: number });
-  },
-};
-
-module.exports = dice;
