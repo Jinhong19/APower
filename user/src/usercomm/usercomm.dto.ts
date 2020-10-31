054120df9ee4dbbc3165cf8e18fe67507e9c2ef9
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-10-30 21:00:17 -0400

    remove all file other than charService

--- user/src/usercomm/usercomm.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, MinLength, MaxLength, IsEmail, IsEnum, IsInt } from 'class-validator';
-
-export class CreateUsercommDto {
-    @IsNotEmpty()
-    user_id: string;
-  
-    @IsNotEmpty()
-    comm_id: string;
-  }
