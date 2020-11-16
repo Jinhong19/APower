db6e87cfd96164cfa6565d17b5f80d41c98d52a4
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2020-11-15 22:22:15 -0500

    a

--- communityService/db.js
+++ communityService/db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-var url = 'mongodb+srv://apower:apowerpassword@cluster1.ieadu.mongodb.net/community?retryWrites=true&w=majority';
-=======
 require('dotenv').config()
 
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.ieadu.mongodb.net/community?retryWrites=true&w=majority`;
->>>>>>> f033399858c1f642126081720b8ec4f5b733def2
 
 mongoose.connect(url, {
     useNewUrlParser: true,
